c1c415db10fb1685be51826b150587d2bad02314
Sathwikaerra <115003764+Sathwikaerra@users.noreply.github.com>
2024-05-30 19:46:26 +0530

    5 commit

--- back/index.js
+++ back/index.js
@@ -25,7 +25,7 @@ app.use('/admin',adminRouter)
 app.use('/movie',movieRouter)
 app.use('/booking',bookingRouter)
 app.use('/ticket',ticketRouter)
-app.use(express.static(path.join(__dirname, 'front/build')));
+// app.use(express.static(path.join(__dirname, 'front/build')));
 
 app.use('/api/hello',(req,res,next)=>{
 res.send("hiii")
