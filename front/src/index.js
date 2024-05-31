ab7b2aceed466a6b775bd2c78b0fd4a4c3f6ccc9
Sathwikaerra <115003764+Sathwikaerra@users.noreply.github.com>
2024-05-31 10:55:47 +0530

    fnl111 commit

--- front/src/index.js
+++ front/src/index.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 import {Provider} from 'react-redux'
 import { store } from './store';
 
-axios.defaults.baseURL='http://localhost:3000/';
+// axios.defaults.baseURL='http://localhost:3000/';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
